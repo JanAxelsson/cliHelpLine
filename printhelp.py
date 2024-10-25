1e86040e217652862f90b5c16143596efd060125
Jan Axelsson <axelsson.jan@gmail.com>
2024-10-25 21:37:05 +0200

    Python -- remove initial character after #
    A header-line starting with '# abc' will display 'abc'

--- printhelp.py
+++ printhelp.py
@@ -10,28 +10,23 @@
 # python printhelp.py --help
 #
 # more help text
-#
-# ./printhelp.py hello
-# ./printhelp.py -h
-# ./printhelp.py --help
-#
-# even more help text
 
 ## Help -- print file header until '##', ignoring first row, removing leading '#'
 import os, sys
 if len(sys.argv) > 1 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
-    print(*open(os.path.realpath(__file__), 'r', encoding='utf-8').read().split('##')[0].split('\n#')[1:], sep='\n'); quit()
+    f = open(os.path.realpath(__file__), 'r') .read() .split('##')[0] .split('\n#')[1:]; print( "\n".join( [ i[1:] for i in f ] ) );quit()
 # End Help
 
 
 # Explanation
-# import os, sys                                # Import from Python Standard Library
+# import os, sys                                    # Import from Python Standard Library
 # if len(sys.argv) > 1 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):    # Run statement only if non-empty first argument is -h or --help
-#    result1= open( os.path.realpath(__file__), 'r', encoding='utf-8' ).read()  # Read current script file
-#    result2 = result1.split('##')[0]           # Keep part before '##'
-#    result3 = result2.split('\n#')[1:]         # Split at character '#' at beginning of line (gives list), and keep all but first line
-#    print(*result3, sep='\n' )                 # Print list items on new rows (\n was removed in previous step)
-#    quit()                                     # Exit script after printing help
+#    result1= open( os.path.realpath(__file__), 'r').read()  # Read current script file
+#    result2 = result1.split('##')[0]               # Keep part before '##'
+#    f = result2.split('\n#')[1:]                   # Split at character '#' at beginning of line (gives list), and keep all but first line
+#    result3 = "\n".join( [ i[1:] for i in f ])     # Skipping first character for each line, and join with end-of-lines
+#    print( result3 )                               # Print 
+#    quit()                                         # Exit script after printing help
 
 
 # Example code
