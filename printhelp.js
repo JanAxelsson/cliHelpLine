dceb0f347d1bf0c154741e08fad68213d49dde01
Jan Axelsson <axelsson.jan@gmail.com>
2024-10-25 22:06:39 +0200

    Javascript - remove empty lines, and initial space after //

--- printhelp.js
+++ printhelp.js
@@ -13,15 +13,18 @@
 
 //// Help -- print file header until '////', ignore first row, remove leading '//'
 if ( (process.argv[2] === "--help") || ( process.argv[2] ===  "-h" ) ){
-    console.log(require("fs").readFileSync(__filename).toString().split(`////`)[0].replace(/^\/\//gm,'')); process.exit();
+    console.log( require("fs").readFileSync(__filename).toString().replace(/^.*\n/, '').split(`////`)[0].replace(/^\s*[\r\n]/gm, '').replace(/^\/\//gm,'').replace(/^ /gm,'') ); process.exit();
 }// End Help
 
 
 // Explanation
 // if ( (process.argv[2] === "--help") || ( process.argv[2] ===  "-h" ) ){   // Run statement only if first argument being -h or --help
 //    require("fs").readFileSync(__filename).toString()                      // Read file to string
+//    .replace(/^.*\n/, '')                                                  // Remove first line
 //    .split(`////`)[0]                                                      // Split at '////'
-//    .replace(/^\/\//gm,'')                                                 // Replace leading // with ''
+//    .replace(/^\s*[\r\n]/gm, '')                                           // Remove blank lines
+//    .replace(/^\/\//gm,'')                                                 // Remove leading '//' 
+//    .replace(/^ /gm,'')                                                    // Remove leading ' '
 //    process.exit();                                                        // Exit script after printing help
 // }
 
