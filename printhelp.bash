b231825dcd9450cf9e3e0f6325b62db7b2baca46
Jan Axelsson <axelsson.jan@gmail.com>
2024-10-25 16:09:58 +0200

    First commit

--- printhelp.bash
+++ printhelp.bash
@@ -5,14 +5,13 @@
 # 
 # Example help text
 #
-# ./printhelp.bash hello
 # ./printhelp.bash -h
 # ./printhelp.bash --help
 #
 # help text
 
 ## Help -- print file header until '##', ignore first row, remove leading '#'
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then sed '1d; /^##/q; s/^#//g' $0 | sed '$d'; exit 0; fi 
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then sed '1d; /^##/q; s/^#//g; s/^ //g' $0 | sed '$d'; exit 0; fi 
 ## End Help
 
 
@@ -20,7 +19,8 @@ if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then sed '1d; /^##/q; s/^#//g' $0 |
 # if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then   # Run statement only if first argument being -h or --help
 #    sed '1d;       # Delete first line
 #    /^##/q;        # Discard everything from line starting with ##
-#    s/^#//g'       # Replace # at beginning of line with ''
+#    s/^#//g;       # Replace # at beginning of line with ''
+#    s/^ //g        # Replace ' ' at new beginning of line with ''
 #    | sed '$d'     # Delete last line of  result
 #    exit 0         # Exit script after help was printed 
 # fi 
